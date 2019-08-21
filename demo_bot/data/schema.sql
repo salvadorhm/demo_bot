32e0e0435f1124133373704d88e8084f6826cecd
Salvador Hernández Mendoza <salvadorhm@gmail.com>
2019-08-21 12:14:54 -0500

    DB update

--- demo_bot/data/schema.sql
+++ demo_bot/data/schema.sql
@@ -2,14 +2,6 @@ CREATE DATABASE samm_db;
 
 USE samm_db;
 
-CREATE TABLE telegram_users(
-    id_telegram_user integer NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    id_telegram integer(15) not null,
-    name varchar(50) NOT NULL,
-    email varchar(50) NOT NULL
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 CREATE TABLE productos(
   id_producto int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   sku varchar(10) NOT NULL,
@@ -18,16 +10,6 @@ CREATE TABLE productos(
   existencias int(11) NOT NULL,
   update_data timestamp
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE answers_logs(
-    id_answer_log int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username varchar(10) NOT NULL,
-    first_name varchar(10) NOT NULL,
-    last_name varchar(10) NOT NULL,
-    id varchar(10) NOT NULL,
-    sku varchar(10) NOT NULL,
-    time_update timestamp
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO productos(sku, producto, precio, existencias)
 VALUES
